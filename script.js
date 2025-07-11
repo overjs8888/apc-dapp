ec563146e480ca02aa4e3c9a5a638b58b1a81eb6
overjs8888 <americapartycoin123@gmail.com>
2025-07-12 04:18:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const APC_ABI = [
     inputs: [{ name: "_owner", type: "address" }],
     name: "balanceOf",
     outputs: [{ name: "balance", type: "uint256" }],
-    type: "function"
+    type: "function",
   },
   {
     constant: true,
     inputs: [],
     name: "decimals",
     outputs: [{ name: "", type: "uint8" }],
-    type: "function"
-  }
+    type: "function",
+  },
 ];
 
 let userAccount;
